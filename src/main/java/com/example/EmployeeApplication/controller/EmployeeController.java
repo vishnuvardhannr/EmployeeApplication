b3d326053e6fafb4e644296f0d3fc871012205d5
Rayarakula Vishnuvardhan <r.vishnuvardhan@globallogic.com>
2024-06-27 11:24:40 +0530

    firstCommit

---
+++ src/main/java/com/example/EmployeeApplication/controller/EmployeeController.java
@@ -0,0 +1,18 @@
+package com.example.EmployeeApplication.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/emp")
+public class EmployeeController {
+
+    @GetMapping("/employee")
+    public ResponseEntity<String> getEmp(){
+        return new ResponseEntity<>("Emp details", HttpStatus.OK);
+    }
+
+}
